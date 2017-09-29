678b635747b071dce0b71d78c854a1b325ec1339
Leonardo Lopez <llopez@rsimail.com>
2017-09-28 22:17:14 -0700

    This is a commit for the data parser.

---
+++ src/com/dataparser/DataParser.java
@@ -0,0 +1,14 @@
+package com.dataparser;
+
+public class DataParser {
+
+	public DataParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi this is the Movie Recommender Data Parser.");
+	}
+
+}
