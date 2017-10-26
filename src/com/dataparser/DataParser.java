6db150984e75798a89229f2d45149079224ba9d1
Leonardo Lopez <leonardolopez19@gmail.com>
2017-10-25 21:57:25 -0700

    Added files to read from and tested printing out one file to console

--- src/com/dataparser/DataParser.java
+++ src/com/dataparser/DataParser.java
@@ -1,5 +1,12 @@
 package com.dataparser;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.Paths;
+
 public class DataParser {
 
 	public DataParser() {
@@ -9,6 +16,41 @@ public class DataParser {
 	public static void main(String[] args) {
 
 		System.out.println("Hi this is the Movie Recommender Data Parser.");
+		String fileName = "C:\\Users\\Leo\\git\\CSC204Project\\Rotten Tomatos Dataset\\movie_actors.dat";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+            	
+                System.out.println(line);
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+		
 	}
 
 }
