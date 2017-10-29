15995c28befb71ea7418b268b2641daff9025da3
Leonardo Lopez <leonardolopez19@gmail.com>
2017-10-28 18:44:39 -0700

    Finished DB schema, saved to /Scripts folder. Now working on connecting to DB with jdbc driver

--- Database Scripts/database schema creation script.sql
+++ Database Scripts/database schema creation script.sql
@@ -1,4 +1,4 @@
-create schema movie_recommender;
+-- create schema movie_recommender;
 
 CREATE TABLE `movie_recommender`.`movies` (
   `movieID` INT NOT NULL,
@@ -28,10 +28,99 @@ CREATE TABLE `movie_recommender`.`movie_genres` (
   `genre` VARCHAR(100) NOT NULL,
   `movieID` INT NOT NULL,
   PRIMARY KEY (`genre`,`movieID`),
-  CONSTRAINT fk_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+  CONSTRAINT fk_movie_genres_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
 
-CREATE TABLE `movie_recommender`.`movie_genres` (
-  `genre` VARCHAR(100) NOT NULL,
+CREATE TABLE `movie_recommender`.`movie_countries` (
+  `country` VARCHAR(100) NOT NULL,
   `movieID` INT NOT NULL,
-  PRIMARY KEY (`genre`,`movieID`),
-  CONSTRAINT fk_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+  PRIMARY KEY (`country`,`movieID`),
+  CONSTRAINT fk_movie_countries_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+  
+CREATE TABLE `movie_recommender`.`movie_actors` (
+  `actorID` VARCHAR(100) NOT NULL,
+  `movieID` INT,
+  `actorName` VARCHAR(200),
+  `ranking` INT,
+  PRIMARY KEY (`actorID`),
+  CONSTRAINT fk_movie_actors_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+  
+CREATE TABLE `movie_recommender`.`movie_directors` (
+  `directorID` VARCHAR(100) NOT NULL,
+  `movieID` INT,
+  `actorName` VARCHAR(200),
+  PRIMARY KEY (`directorID`),
+  CONSTRAINT fk_movie_directors_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));  
+  
+CREATE TABLE `movie_recommender`.`tags` (
+  `tagID` INT NOT NULL,
+  `value` VARCHAR(200),
+  PRIMARY KEY (`tagID`));
+
+CREATE TABLE `movie_recommender`.`user_tagged_movies` (
+  `userID` INT NOT NULL,
+  `movieID` INT NOT NULL,
+  `tagID` INT NOT NULL,
+  `date_day` INT,
+  `date_month` INT,
+  `date_year` INT,
+  `date_hour` INT,
+  `date_minute` INT,
+  `date_second` INT,
+  PRIMARY KEY (`userID`, `movieID`, `tagID`),
+  CONSTRAINT fk_user_tagged_movies_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID), 
+  CONSTRAINT fk_user_tagged_movies_tagID FOREIGN KEY(tagID) REFERENCES tags(tagID));  
+  
+CREATE TABLE `movie_recommender`.`user_tagged_movies_timestamps` (
+  `userID` INT NOT NULL,
+  `movieID` INT NOT NULL,
+  `tagID` INT NOT NULL,
+  `timestamp` INT,
+  PRIMARY KEY (`userID`, `movieID`, `tagID`),
+  CONSTRAINT fk_user_tagged_movies_timestamps_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID), 
+  CONSTRAINT fk_user_tagged_movies_timestamps_tagID FOREIGN KEY(tagID) REFERENCES tags(tagID));
+
+CREATE TABLE `movie_recommender`.`movie_tags` (
+  `movieID` INT NOT NULL,
+  `tagID` INT NOT NULL,
+  `tagWeight` INT,
+  PRIMARY KEY (`movieID`, `tagID`),
+  CONSTRAINT fk_movie_tags_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID), 
+  CONSTRAINT fk_movie_tags_tagID FOREIGN KEY(tagID) REFERENCES tags(tagID)); 
+
+CREATE TABLE `movie_recommender`.`user_rated_movies` (
+  `userID` INT NOT NULL,
+  `movieID` INT NOT NULL,
+  `rating` DECIMAL(2,1) NULL,
+  `date_day` INT,
+  `date_month` INT,
+  `date_year` INT,
+  `date_hour` INT,
+  `date_minute` INT,
+  `date_second` INT,
+  PRIMARY KEY (`userID`, `movieID`),
+  CONSTRAINT fk_user_rated_movies_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+  
+CREATE TABLE `movie_recommender`.`user_rated_movies_timestamps` (
+  `userID` INT NOT NULL,
+  `movieID` INT NOT NULL,
+  `rating` DECIMAL(2,1) NULL,
+  `timestamp` INT,
+  PRIMARY KEY (`userID`, `movieID`),
+  CONSTRAINT fk_user_rated_movies_timestamps_movieID FOREIGN KEY(movieID) REFERENCES movies(movieID));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
