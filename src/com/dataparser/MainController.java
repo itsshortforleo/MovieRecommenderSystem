b98ded92971a23c1e2afec35c1c4ab7d91fabcb7
Nuha <Nuha@nuhas-mbp.lan>
2017-11-08 06:00:23 -0800

    More modifications..

--- src/com/dataparser/MainController.java
+++ src/com/dataparser/MainController.java
@@ -2,40 +2,65 @@ package com.dataparser;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MainController {
-	public void search(){
-		System.out.print("inside controller");
+	@FXML private TextField searchText;
+	List<Result> results= new LinkedList();
+
+	public  List Query2(){
 		final String DB_URL = "jdbc:mysql://localhost:3306/movie_recommender?useSSL=false";
 		
 		// Database Credentials
 		final String USER = "root";
 		final String PASS = "nuha1410";
-
+		
+		String searchParameter=searchText.getText();
 		try {
 			// 1. Get a connection to database
 			Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			
-			// 2. Create a statement
-			Statement stmt = conn.createStatement();
+			// 2. Create a statement & Execute SQL query
+			String query1="SELECT m.`title`, m.`year`, m.`rtAudienceScore`, m.`rtPictureURL`, m.`imdbPictureURL` FROM `movies` m ORDER BY m.`rtAudienceScore` LIMIT 5;";
+			String query2="SELECT m.`title`, m.`year`, m.`rtAudienceScore`, m.`rtPictureURL`, m.`imdbPictureURL`, t.`value` FROM `tags` t,`movies` m, `user_tagged_movies` ut WHERE m.`movieID`=ut.`movieID` AND t.`tagID`=ut.`tagID` AND m.`title` LIKE ?;";
+			String query3="SELECT m.`title`,m.`year`, m.`rtAudienceScore`, m.`rtPictureURL`, m.`imdbPictureURL` FROM `movies` m, `movie_genres` mg  WHERE m.`movieID`=mg.`movieID` AND mg.`genre` LIKE '%Adventure%' ORDER BY m.`rtAudienceScore`  LIMIT 5;";
+			String query4="SELECT m.`title`,m.`year`, m.`rtAudienceScore`, m.`rtPictureURL`, m.`imdbPictureURL` FROM `movies` m, `movie_directors` md  WHERE m.`movieID`=md.`movieID` AND md.`directorName` LIKE '%John Lasseter%';";
+			String query5="SELECT m.`title`,m.`year`, m.`rtAudienceScore`, m.`rtPictureURL`, m.`imdbPictureURL` FROM `movies` m, `movie_actors` ma WHERE m.`movieID`=ma.`movieID` AND ma.`actorName` LIKE '%Annie Potts%';";
+
 			
-			// 3. Execute SQL query
-			ResultSet myRs = stmt.executeQuery("select * from movie_actors");
+			PreparedStatement ps=conn.prepareStatement(query2);
+			ps.setString(1, "%"+searchParameter+"%");
+			ResultSet myRs = ps.executeQuery();
 			
 			// 4. Process the result set
 			while(myRs.next()) {
-				System.out.println(myRs.getString("actorName"));
+				System.out.println(myRs.getString("title"));
+				Result result= new Result();
+				result.setTitle(myRs.getString("title"));
+				result.setYear(myRs.getInt("year"));
+				result.setRtAudienceScore(myRs.getInt("rtAudienceScore"));
+				result.setImdbPictureURL(myRs.getString("imdbPictureURL"));
+				result.setRtPictureURL(myRs.getString("rtPictureURL"));
+				
+				results.add(result);
+				
 			}
 			
-			stmt.close();
+			myRs.close();
+			ps.close();
 			conn.close();
 		}
 		catch (Exception e){
 			
 			e.printStackTrace();
-		}		
+		}	
+		return results;
 	}
 
 }
---
+++ src/com/dataparser/Result.java
@@ -0,0 +1,50 @@
+package com.dataparser;
+
+public class Result {
+	private String  title;
+	private int year;
+	private int rtAudienceScore;
+	private String  rtPictureURL;
+	private String imdbPictureURL;
+	//get methods
+	public int getYear(){
+		return year;
+	}
+	
+	public int getRtAudienceScore(){
+		return rtAudienceScore;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public String getRtPictureURL(){
+		return rtPictureURL;
+	}
+	
+	public String getImdbPictureURL(){
+		return imdbPictureURL;
+	}
+	// set methods
+	public void setYear(int year){
+		this.year= year;
+	}
+	
+	public void setRtAudienceScore(int rtAudienceScore){
+		this.rtAudienceScore= rtAudienceScore;
+	}
+	
+	public void setTitle(String title){
+		this.title= title;
+	}
+	
+	public void setRtPictureURL(String rtPictureURL){
+		this.rtPictureURL= rtPictureURL;
+	}
+	
+	public void setImdbPictureURL(String imdbPictureURL){
+		this.imdbPictureURL= imdbPictureURL;
+	}
+
+}
